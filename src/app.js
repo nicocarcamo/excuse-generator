0a7c22343f4fea0d96444dd6f9ffc4e4bec1996c
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2022-08-26 14:46:28 +0000

    excuse-generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "My fish", "The cat"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when i finished",
+  "during my lunch"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let firstString = who[Math.floor(Math.random() * who.length)];
+  let secondString = action[Math.floor(Math.random() * action.length)];
+  let thirdString = what[Math.floor(Math.random() * what.length)];
+  let fourthString = when[Math.floor(Math.random() * when.length)];
+  document.getElementById(
+    "excuse"
+  ).innerHTML = `${firstString} ${secondString} ${thirdString} ${fourthString}`;
 };
